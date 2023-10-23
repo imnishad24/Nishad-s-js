91d6cb359965b088c8f7c93777fb9487bb2e36ff
imnishad24 <147955411+imnishad24@users.noreply.github.com>
2023-10-23 09:47:38 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14453;
+let accountEmail = "imnishad24@gmail.com";
+var accountPassword = "23456";
+accountCity = "Feni";
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "nishadrifat01.com";
+accountPassword = "232323";
+accountCity = "Dhaka";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 +3); // We are using nodejs, not browser
+
+//console.log(3 +3);
+
+//console.log("Nishad");
+
+let name = "nishad";
+let age = 24 ;
+let isLoggedIn = false;
+let state;
+
+//console.log(typeof state);
+//console.log(typeof null);
+//console.log(typeof undefined);
+//console.log(typeof age);
+
+// number => 2 to power 53
+// bigint   
+// string => ""
+// boolean => true/false
+// null => standalone value //object
+// undefined =>
+// symbol => unique
