919016432d05a3b88300d6d8bf8457f77ba8bf15
imnishad24 <147955411+imnishad24@users.noreply.github.com>
2023-10-23 17:26:09 +0000

    Conversion of DataType

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+/* let score = false ;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber); 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+
+let isLoggedIn = "Nishad" ; 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);  */
+
+// 1 => true; 0 => false;
+// "" => false; 
+// "Nishad" = true;
+
+let sum = 33 ;
+
+let stringNumber = String(sum);
+console.log(stringNumber);
+console.log(typeof stringNumber);
