399c0765bf69bce79aa5d6c9b2a1ac349b1fd7d9
imnishad24 <147955411+imnishad24@users.noreply.github.com>
2023-10-24 06:04:04 +0000

    conversionOperation

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 let isLoggedIn = "Nishad" ; 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);  */
+console.log(booleanIsLoggedIn);  
 
 // 1 => true; 0 => false;
 // "" => false; 
@@ -25,4 +25,45 @@ let sum = 33 ;
 
 let stringNumber = String(sum);
 console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);  */
+
+// ******************* Operations ************************
+
+/* let value = 3 ;
+let negValue = -3 ;
+// console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*4);
+console.log(2**4);
+console.log(2/5);
+console.log(2%4);  
+
+let str1 = "Hello" ;
+let str2 = " Nishad" ;
+
+let str3 = str1 + str2 ;
+console.log(str3);
+
+console.log("1" + 3) ;
+console.log(1 + "3") ;
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");  
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true) ;
+console.log(+"") ; */
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 ;
+
+let gameCounter = 100 ;
+++gameCounter ;
+console.log(gameCounter) ;
+
+// Link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html 
+
