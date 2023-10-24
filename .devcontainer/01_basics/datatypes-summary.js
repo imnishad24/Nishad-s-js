c474543b32b54d6661fb2cd6823f069ae8b4597f
imnishad24 <147955411+imnishad24@users.noreply.github.com>
2023-10-24 11:31:47 +0000

    dataTypes-Summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,43 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt;
+
+const score = 100;
+const scoreValue = 100.3;
+//console.log(typeof scoreValue);
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+//console.log(typeof outsideTemp);
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+//console.log(typeof id);
+console.log(typeof anotherId);
+
+const bigNumber = 12345678910n;
+//console.log(12345678910n);
+//console.log(typeof bigNumber);
+
+// console.log(id === anotherId); 
+
+
+// Reference (Non primitive);
+
+// Array, Object, Functions
+
+const heros = ["Abu Bakar", "Omar", "Osman"];
+let myObj = {
+    name : "Nishad",
+    age: 24,
+}
+//console.log(typeof heros);
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+//console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
