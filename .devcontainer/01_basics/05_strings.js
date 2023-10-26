7f036ba780bbbf8fe53c31e65d7ef0ad122367a8
imnishad24 <147955411+imnishad24@users.noreply.github.com>
2023-10-26 16:38:11 +0000

    strings

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,36 @@
+const name = "nishad";
+const repoCount = 24;
+
+//console.log(name + repoCount + " value");
+
+//console.log(`Hi my name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String('daudnishad.com');
+
+//console.log(gameName[2]);
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.indexOf("s"));
+//console.log(gameName.charAt(2));
+
+const newString = gameName.substring(-5, 4);
+//console.log(newString);
+
+const anotherString = gameName.slice(-2, 4);
+//console.log(anotherString);
+
+const newStringOne = "      Nishad      ";
+//console.log(newStringOne);
+//console.log(newStringOne.trim());
+//console.log(newStringOne.trimEnd());
+//console.log(newStringOne.trimStart());
+
+const url = "https://daudnishad.com/daud%20nishad"
+
+//console.log(url.replace('%20', '-'));
+//console.log(url.includes('rifat'));
+
+console.log(gameName.split(' '));
+console.log(gameName.split('-'));
